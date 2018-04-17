acf68ba441d7e126c21c8cab664361e4c349793d
davidcoroian <david.coroian@code.berlin>
2018-04-17 16:15:13 +0200

    added route /moods, to be able to access db data from js

--- routes/botman.php
+++ routes/botman.php
@@ -7,6 +7,7 @@ $botman = resolve('botman');
 //$botman->hears('hi', function (Botman $bot) {
 //    $bot->reply('Hello!');
 //});
+
 $botman->hears('hi|hello|mood|hello there', BotManController::class.'@startMoodConversation');
 
 $botman->hears('location', BotManController::class.'@startLocationConversation');
